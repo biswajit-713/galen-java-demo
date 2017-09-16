c0719af893175d08c951eda18f8d36ab2cda7240
biswajit-713 <biswajit.pattanayak@gmail.com>
2017-09-16 11:23:12 +0530

    galen-java-script
    galen-java-demo

--- src/test/java/com/galenframework/java/sample/components/GalenTestBase.java
+++ src/test/java/com/galenframework/java/sample/components/GalenTestBase.java
@@ -3,41 +3,63 @@ package com.galenframework.java.sample.components;
 import com.galenframework.testng.GalenTestNgTestBase;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.DataProvider;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 
 public abstract class GalenTestBase extends GalenTestNgTestBase {
 
-    private static final String ENV_URL = "http://testapp.galenframework.com";
+    private static final String USER_NAME = "biswajitpattanay1";
+    private static final String API_KEY = "s1xiWnqpZZ2xyoCntHoj";
+    //private static final String ENV_URL = "https://" + USER_NAME + ":" + API_KEY + "@hub-cloud.browserstack.com/wd/hub";
+    private static final String ENV_URL = "http://testapp.galenframework.com/";
+    //private static final String APP_URL = "http://testapp.galenframework.com/";
+    private static final String APP_URL = "http://devint.neimanmarcus.com";
 
     @Override
     public WebDriver createDriver(Object[] args) {
-        WebDriver driver = new FirefoxDriver();
-        if (args.length > 0) {
-            if (args[0] != null && args[0] instanceof TestDevice) {
-                TestDevice device = (TestDevice)args[0];
-                if (device.getScreenSize() != null) {
-                    driver.manage().window().setSize(device.getScreenSize());
+        //WebDriver driver = new ChromeDriver();
+        DesiredCapabilities caps = DesiredCapabilities.firefox();
+
+        try {
+            //WebDriver driver = new RemoteWebDriver(new URL(ENV_URL), caps);
+            WebDriver driver = new ChromeDriver();
+            if (args.length > 0) {
+                if (args[0] != null && args[0] instanceof TestDevice) {
+                    TestDevice device = (TestDevice)args[0];
+                    if (device.getScreenSize() != null) {
+                        driver.manage().window().setSize(device.getScreenSize());
+                    }
                 }
             }
+            return driver;
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+            return null;
         }
-        return driver;
+
     }
 
     public void load(String uri) {
-        getDriver().get(ENV_URL + uri);
+        getDriver().get(APP_URL + uri);
     }
 
     @DataProvider(name = "devices")
     public Object [][] devices () {
         return new Object[][] {
+/*
                 {new TestDevice("mobile", new Dimension(450, 800), asList("mobile"))},
                 {new TestDevice("tablet", new Dimension(750, 800), asList("tablet"))},
-                {new TestDevice("desktop", new Dimension(1024, 800), asList("desktop"))}
+*/
+                {new TestDevice("desktop", new Dimension(1366, 800), asList("desktop"))}
         };
     }
 
--- src/test/java/com/galenframework/java/sample/tests/WelcomePageTest.java
+++ src/test/java/com/galenframework/java/sample/tests/WelcomePageTest.java
@@ -1,15 +1,17 @@
 package com.galenframework.java.sample.tests;
 
 import com.galenframework.java.sample.components.GalenTestBase;
-import com.galenframework.java.sample.components.GalenTestBase;
-import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.Date;
 
 
 public class WelcomePageTest extends GalenTestBase {
 
+/*
     @Test(dataProvider = "devices")
     public void welcomePage_shouldLookGood_onDevice(TestDevice device) throws IOException {
         load("/");
@@ -21,6 +23,42 @@ public class WelcomePageTest extends GalenTestBase {
         load("/");
         getDriver().findElement(By.xpath("//button[.='Login']")).click();
         checkLayout("/specs/loginPage.spec", device.getTags());
+    }
+*/
+
+    @Test(dataProvider = "devices")
+    public void homePage_EmailSection_onDevice(TestDevice device) throws IOException {
+        load("/");
+        Cookie intlCookie = new Cookie.Builder("COUNTRY_SELECTOR", "true")
+                .domain(".neimanmarcus.com")
+                .expiresOn(new Date(2019, 10, 28))
+                .isHttpOnly(true)
+                .isSecure(false)
+                .path("/")
+                .build();
+        Cookie homePageCookie = new Cookie.Builder("ihp", "DTT5")
+                .domain(".neimanmarcus.com")
+                .expiresOn(new Date(2019, 10, 28))
+                .isHttpOnly(true)
+                .isSecure(false)
+                .path("/")
+                .build();
+
+        getDriver().manage().addCookie(intlCookie);
+        getDriver().manage().addCookie(homePageCookie);
+        load("/");
+
+        try {
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        //getDriver().navigate().refresh();
+        JavascriptExecutor js = (JavascriptExecutor)getDriver();
+        js.executeScript("window.scrollTo(0, window.outerHeight)");
+        checkLayout("/specs/internationalHomePage.gspec", device.getTags());
     }
 
 }
