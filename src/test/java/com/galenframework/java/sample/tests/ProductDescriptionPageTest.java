6adb89289a4d5ec97d9ad0be36be7ae003bacd47
biswajit <biswajit.pattanayak@gmail.com>
2018-02-03 15:53:06 +0530

    test for PDP page

---
+++ src/test/java/com/galenframework/java/sample/tests/ProductDescriptionPageTest.java
@@ -0,0 +1,21 @@
+package com.galenframework.java.sample.tests;
+
+import com.galenframework.java.sample.components.GalenTestBase;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+/**
+ * Created by biswajip on 2/3/18.
+ */
+public class ProductDescriptionPageTest extends GalenTestBase {
+
+    @Test(dataProvider = "devices")
+    public void Product_Image_onDevice(TestDevice device) throws IOException{
+        load("/Bogner-Down-Puffer-Vest-Neon-Red/prod192090050/p.prod");
+        JavascriptExecutor js = (JavascriptExecutor)getDriver();
+        js.executeScript("window.scrollTo(0, window.outerHeight)");
+        checkLayout("/specs/ProductDescriptionPage.gspec", device.getTags());
+    }
+}
