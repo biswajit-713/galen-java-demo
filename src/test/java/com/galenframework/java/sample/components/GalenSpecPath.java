849163121f83d4a620d0acc468c5bea3963d9da6
biswajit <biswajit.pattanayak@gmail.com>
2018-02-28 15:24:12 +0530

    spec path repository

---
+++ src/test/java/com/galenframework/java/sample/components/GalenSpecPath.java
@@ -0,0 +1,12 @@
+package com.galenframework.java.sample.components;
+
+
+/**
+ * Created by biswajip on 2/23/18.
+ */
+public class GalenSpecPath {
+    public static final String INTL_HOME_PAGE_SPEC = "/specs/internationalHomePage.gspec";
+    public static final String COUNTRY_CHANGER_MODAL_SPEC = "/specs/changeCountryModal.gspec";
+    public static final String QUICK_LOOK_SPEC = "/specs/QuickLook.gspec";
+    public static final String PDP_SPEC = "/specs/ProductDescriptionPage.gspec";
+}
