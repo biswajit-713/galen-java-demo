5d2387c9a96e16a14c3594c3af25768f6a1404ad
raiak82 <akhlesh.rai@gmail.com>
2018-03-14 16:40:21 +0530

    don't run the test

--- src/test/java/com/galenframework/java/sample/tests/ProductListPageTest.java
+++ src/test/java/com/galenframework/java/sample/tests/ProductListPageTest.java
@@ -1,23 +1,15 @@
 package com.galenframework.java.sample.tests;
 
-import com.galenframework.java.sample.components.*;
 import com.galenframework.java.sample.pageObjects.ProductListPage;
 import com.galenframework.java.sample.pageObjects.QuickLook;
-import lombok.SneakyThrows;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.Test;
 
+import com.galenframework.java.sample.components.*;
+import org.testng.annotations.Test;
 import java.io.IOException;
 
-/**
- * Created by biswajip on 2/21/18.
- */
 public class ProductListPageTest extends GalenTestBase {
 
-    @Test(dataProvider = "devices", retryAnalyzer = RetryAnalyzer.class, enabled = true)
+    @Test(dataProvider = "devices", retryAnalyzer = RetryAnalyzer.class, enabled = false)
     public void Quick_Look_onDevice(TestDevice device) throws IOException {
         load(Config.PLP_PAGE);
         BoilerPlate.RegisterCookie(getDriver());
